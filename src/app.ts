77d2f4d374f50e19a4755dce7a3e48da75e35872
ThePicoNerd <ake.amcoff@lynx.agency>
2021-06-07 17:15:44 +0200

    fix(proxy): CORS

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import fastify, { FastifyInstance, FastifyServerOptions } from "fastify";
 import { IncomingMessage, Server, ServerResponse } from "http";
 import Etag from "fastify-etag";
+import fastifyCors from "fastify-cors";
 import { routes as menuRoutes } from "./menus";
 
 const build = (opts: FastifyServerOptions = {}): FastifyInstance<Server, IncomingMessage, ServerResponse> => {
 	const app: FastifyInstance = fastify({
 		ignoreTrailingSlash: true,
 		...opts,
+	});
+
+	app.register(fastifyCors, {
+		origin: "*",
 	});
 
 	app.register(Etag);
