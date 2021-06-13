cc1ee90033403530309380e996a7a8241a1cf262
ThePicoNerd <ake@amcoff.net>
2021-06-13 14:42:03 +0200

    feat(proxy): Add compression

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import fastify, { FastifyInstance, FastifyServerOptions } from "fastify";
 import { IncomingMessage, Server, ServerResponse } from "http";
 import Etag from "fastify-etag";
 import fastifyCors from "fastify-cors";
+import fastifyCompress from "fastify-compress";
 import { routes as menuRoutes } from "./menus";
 
 const build = (opts: FastifyServerOptions = {}): FastifyInstance<Server, IncomingMessage, ServerResponse> => {
@@ -15,6 +16,8 @@ const build = (opts: FastifyServerOptions = {}): FastifyInstance<Server, Incomin
 	});
 
 	app.register(Etag);
+
+	app.register(fastifyCompress);
 
 	app.get("/health", async (_, reply) => {
 		return reply.header("Cache-Control", "no-cache").send("Поехали!"); // Russian for "let's go!"
