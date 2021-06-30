e2ccbef0cec6cd0d56b807e94aa2b83c8ba721f0
ThePicoNerd <ake@amcoff.net>
2021-06-30 21:08:08 +0200

    fix(proxy): Trim school names

--- tests/utils/polish-meals.spec.ts
+++ tests/utils/polish-meals.spec.ts
@@ -1,22 +1,9 @@
 import { Meal } from "../../src/types";
-import { polishMeals, polishMealValue } from "../../src/utils/polish-meals";
+import { polishMeals } from "../../src/utils/polish-meals";
 
 function generateMeals(values: string[]): Meal[] {
 	return values.map((value) => ({ value }));
 }
-
-test("polish meal names", () => {
-	const cases: Record<string, string> = {
-		"\t    Fisk Björkeby                   \n": "Fisk Björkeby",
-		"\n\n\nVeg. Potatisbullar.\n\n": "Veg. Potatisbullar",
-		"A     \n..............              ": "A",
-		Tacos: "Tacos",
-	};
-
-	Object.entries(cases).forEach(([input, expected]) => {
-		expect(polishMealValue(input)).toBe(expected);
-	});
-});
 
 test("polish meals", () => {
 	expect(
