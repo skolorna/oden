591be16985ec2623d1eb0a54e3525dd0cfebc358
ThePicoNerd <ake@amcoff.net>
2021-07-07 20:41:51 +0200

    Remove school break messages in meals

--- src/menus/meal.rs
+++ src/menus/meal.rs
@@ -17,14 +17,37 @@ impl Meal {
     /// assert!(Meal::from_value("\n\n\n").is_none());
     /// ```
     pub fn from_value(value: &str) -> Option<Self> {
-        let trimmed = value.trim();
+        let trimmed = value.split_whitespace().collect::<Vec<&str>>().join(" ");
 
-        if trimmed.is_empty() {
-            return None;
+        if trimmed.is_empty() || trimmed.to_lowercase().contains("lov") {
+            None
+        } else {
+            Some(Self {
+                value: trimmed.to_owned(),
+            })
         }
+    }
+}
 
-        Some(Self {
-            value: trimmed.to_owned(),
-        })
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn meal() {
+        assert!(Meal::from_value("      \t\n    ").is_none());
+        assert_eq!(
+            Meal::from_value("              Fisk Björkeby ")
+                .unwrap()
+                .value,
+            "Fisk Björkeby"
+        );
+        assert_eq!(
+            Meal::from_value("Fisk\t\t          Björkeby med ris     \n")
+                .unwrap()
+                .value,
+            "Fisk Björkeby med ris"
+        );
+        assert!(Meal::from_value("\t\n  dET ÄR SommarLOV!!!!!\n\n").is_none());
     }
 }
