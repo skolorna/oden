9fe43aae961df2be231ac55de258568e6c5ae046
ThePicoNerd <ake@amcoff.net>
2021-07-17 16:00:40 +0200

    SWR

--- src/routes/menus/mod.rs
+++ src/routes/menus/mod.rs
@@ -10,13 +10,17 @@ use serde::Deserialize;
 use crate::{
     errors::Result,
     menus::{id::MenuID, list_days, list_menus, query_menu, ListDaysQuery},
+    routes::swr,
 };
 
 /// Route for listing menus.
 async fn list_menus_route() -> Result<HttpResponse> {
     let menus = list_menus().await?;
     let res = HttpResponse::Ok()
-        .set(CacheControl(vec![CacheDirective::MaxAge(86_400)]))
+        .set(CacheControl(vec![
+            CacheDirective::MaxAge(86_400),
+            swr(604_800), // 7 days
+        ]))
         .json(menus);
 
     Ok(res)
@@ -26,7 +30,10 @@ async fn list_menus_route() -> Result<HttpResponse> {
 async fn query_menu_route(web::Path(menu_id): web::Path<MenuID>) -> Result<HttpResponse> {
     let menu = query_menu(&menu_id).await?;
     let res = HttpResponse::Ok()
-        .set(CacheControl(vec![CacheDirective::MaxAge(86_400)]))
+        .set(CacheControl(vec![
+            CacheDirective::MaxAge(604_800), // 7 days
+            swr(2_419_200),                  // 28 days
+        ]))
         .json(menu);
 
     Ok(res)
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,7 +2,7 @@
 
 pub mod menus;
 
-use actix_web::{web, Responder};
+use actix_web::{http::header::CacheDirective, web, Responder};
 
 pub async fn get_health() -> impl Responder {
     "\u{41f}\u{43e}\u{435}\u{445}\u{430}\u{43b}\u{438}!" // "Поехали!", russian for "Let's go!"
@@ -12,6 +12,14 @@ pub async fn get_health() -> impl Responder {
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("/health").route(web::get().to(get_health)))
         .service(web::scope("/menus").configure(menus::configure));
+}
+
+/// `stale-while-revalidate` as a `CacheDirective`.
+pub fn swr(seconds: u32) -> CacheDirective {
+    CacheDirective::Extension(
+        "stale-while-revalidate".to_owned(),
+        Some(seconds.to_string()),
+    )
 }
 
 #[cfg(test)]
@@ -28,5 +36,11 @@ mod tests {
         let resp = test::call_service(&mut app, req).await;
 
         assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[test]
+    fn swr_works() {
+        assert_eq!("stale-while-revalidate=300", swr(300).to_string());
+        assert_eq!("stale-while-revalidate=86400", swr(86_400).to_string());
     }
 }
