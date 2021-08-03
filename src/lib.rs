293fd584728369fa86d5811beb9c7c114901817a
ThePicoNerd <ake@amcoff.net>
2021-08-03 16:06:47 +0200

    Add server header

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,16 @@ pub mod util;
 macro_rules! create_app {
     () => {{
         use actix_cors::Cors;
+        use actix_web::dev::Service;
+        use actix_web::http::header::{self, HeaderValue};
         use actix_web::middleware::{self, normalize};
         use actix_web::App;
+        use const_format::concatcp;
+        use futures::future::FutureExt;
+
         use menu_proxy::routes;
+
+        const SERVER_NAME: &str = concatcp!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 
         App::new()
             .wrap(normalize::NormalizePath::new(
@@ -22,6 +29,15 @@ macro_rules! create_app {
                     .allow_any_origin()
                     .allow_any_method(),
             )
+            .wrap_fn(|req, srv| {
+                srv.call(req).map(|res| {
+                    res.map(|mut res| {
+                        let headers = res.headers_mut();
+                        headers.insert(header::SERVER, HeaderValue::from_str(SERVER_NAME).unwrap());
+                        res
+                    })
+                })
+            })
             .wrap(middleware::Logger::new("%r (%Ts)"))
             .wrap(middleware::Compress::default())
     }};
