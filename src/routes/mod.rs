ac75fb549e1a44e3531877377acfb35f285edabf
ThePicoNerd <ake@amcoff.net>
2021-10-14 20:55:34 +0200

    oops

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -14,7 +14,7 @@ pub async fn get_health() -> impl Responder {
         .body("\u{41f}\u{43e}\u{435}\u{445}\u{430}\u{43b}\u{438}!") // "Поехали!", russian for "Let's go!"
 }
 
-/// Configure all the  routes.
+/// Configure all the routes.
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("/health").route(web::get().to(get_health)))
         .service(web::scope("/menus").configure(menus::configure));
