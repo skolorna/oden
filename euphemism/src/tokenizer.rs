1f1325bc86a28cd8432b9ebf650e23adf09169f4
ThePicoNerd <ake@amcoff.net>
2021-10-31 15:59:28 +0100

    :(

---
+++ euphemism/src/tokenizer.rs
@@ -0,0 +1,35 @@
+const STOP_WORDS: &[&str] = &["med", "serveras"];
+
+/// Tokenize a value, preparing it for further processing.
+///
+/// ```
+/// use euphemism::tokenizer::tokenize;
+///
+/// // These are real-world examples
+/// assert_eq!(tokenize("Köttbullar med makaroner och ketchup"), "Köttbullar");
+/// assert_eq!(tokenize("Köttbullar, stuvade makaroner och grönsaker"), "Köttbullar");
+/// assert_eq!(tokenize("Taco´s nöt med tillbehör (skola)"), "Tacos nöt");
+/// ```
+pub fn tokenize(val: &str) -> String {
+    let mut output = String::new();
+
+    'words: for word in val.split_whitespace() {
+        if STOP_WORDS.contains(&word) {
+            break;
+        }
+
+        for c in word.chars() {
+            if c == ',' {
+                break 'words;
+            }
+
+            if c.is_alphabetic() {
+                output.push(c)
+            }
+        }
+
+        output.push(' ');
+    }
+
+    output.trim().to_owned()
+}
