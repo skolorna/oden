87727434c78c8a3fcd69b6e8147ed49e54606b03
Åke Amcoff <ake@amcoff.net>
2021-11-14 11:02:57 +0100

    Update up.sql

--- database/migrations/2021-11-07-135854_create_days/up.sql
+++ database/migrations/2021-11-07-135854_create_days/up.sql
@@ -1,9 +1,8 @@
 -- Your SQL goes here
 CREATE TABLE days (
-  id SERIAL PRIMARY KEY,
   date DATE NOT NULL,
   meals BYTEA NOT NULL,
   menu_id SERIAL NOT NULL,
-  FOREIGN KEY (menu_id) REFERENCES menus(id) ON DELETE CASCADE,
-  UNIQUE (date, menu_id)
+  PRIMARY KEY (date, menu_id),
+  FOREIGN KEY (menu_id) REFERENCES menus(id) ON DELETE CASCADE
 );
